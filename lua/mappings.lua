3dbecab90c3f90fa5d4c3e58c8feb0ce684bac29
Saul <saul.lopezfdez@gmail.com>
2024-06-15 21:55:45 +0200

    Adding DAP step in

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -31,6 +31,7 @@ map("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP Set breakpoint" })
 map("n", "<leader>dc", dap.continue, { desc = "DAP Run" })
 map("n", "<leader>dO", dap.step_out, { desc = "DAP Step out" })
 map("n", "<leader>do", dap.step_over, { desc = "DAP Step over" })
+map("n", "<leader>di", dap.step_into, { desc = "DAP Step in" })
 local dapui = require "dapui"
 map("n", "<leader>dt", dapui.toggle, { desc = "DAP UI Toggle" })
 map("n", "<leader>dk", dapui.eval, { desc = "DAP eval hover" })
